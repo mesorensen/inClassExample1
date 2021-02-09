0d2a9af16748860e2b4e2cf975eebcc1b65e343d
mesorensen <77695772+mesorensen@users.noreply.github.com>
2021-02-09 13:39:06 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# inClassExample1
---
+++ ShapeApp.java
@@ -0,0 +1,16 @@
+public class ShapeApp{
+public static void main(String[] args){
+
+    Circle hulahoop = new Circle(36);
+    
+    System.out.println(hulahoop.getArea());
+    
+    Square2 picture = new Square2(4);
+    System.out.println(picture.getArea());
+    
+    Rectaqngle2 notebook = new Rectangle(8, 11);
+    System.out.println(notebook.getArea());
+    
+    
+}
+}
